18b186e7cc0f6d798a7a5b14d7c354addc587e08
klaratravnik <travnik.klara@gmail.com>
2021-09-04 11:27:07 +0200

    zacela delo na spletnem vmesniku

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,6 +1,24 @@
 import bottle
+import os
+from model import Model, SeznamReceptov, Recept
 
 
+@bottle.get('/')
+def osnovna_stran():
+    m = nalozi_uporabnikovo_stanje() # dodaj funkcijo za branje modela
+    return bottle.template(
+        'osnovna_stran.html',
+        stPriljubljenih = m.steviloPriljubljenih(),
+        recepti = m.seznamZaPrikaz.recepti if m.seznamZaPrikaz else [],
+        seznami = m.seznamiReceptov,
+        seznamZaPrikaz = m.seznamZaPrikaz,
+        uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime"),
+    )
+
+
+@bottle.error(404)
+def error_404(error):
+    return "Ta stran ne obstaja!"
 
 
 bottle.run(reloader=True, debug=True)
